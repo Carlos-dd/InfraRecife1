2a0f43366c8aaf670c612d1fb68eecb7ecb99c91
userbla <diegods1505@gmail.com>
2023-04-18 20:17:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/empregaueu/Tela3Activity.kt
@@ -0,0 +1,20 @@
+package com.example.empregaueu
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.empregaueu.databinding.ActivitySegundaTelaBinding
+import com.example.empregaueu.databinding.ActivityTela3Binding
+
+class Tela3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela3)
+
+
+
+
+
+        }
+
+    }
